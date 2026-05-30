151142ea9003fa3c21bddafcaeb15879cb64fc2c
Vanshmaan <vanshmaan9459@gmail.com>
2026-05-30 23:42:50 +0530

    fix express wildcard route

--- client/server.js
+++ client/server.js
@@ -2,15 +2,15 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// serve static files from dist
 app.use(express.static("dist"));
 
-// handle React routes
-app.get("*", (req, res) => {
+// FIXED ROUTE FOR EXPRESS 5
+app.get(/.*/, (req, res) => {
   res.sendFile(path.resolve("dist/index.html"));
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
